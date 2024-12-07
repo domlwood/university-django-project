771784ad68cf27847734b67247bb9f25aa0f223f
Dominic Wood <dominic.wood@theapsgroup.com>
2024-12-07 17:50:59 +0000

    chore(ci): update deployment

---
+++ docs/secrets.md
@@ -0,0 +1,3 @@
+secret_key is 
+
+django-insecure-10#at9267=k@$#*w9!$=zafk1@(r(83pb$vgcii#&$l&kbihuo
--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = ['*']
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-10#at9267=k@$#*w9!$=zafk1@(r(83pb$vgcii#&$l&kbihuo'
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # Application definition
